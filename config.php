bbc374050b2cc97e40ce06bba8166890b9599db9
monikagore <monikagore6@gmail.com>
2022-09-01 17:19:39 +0530

    m

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //$conn=mysqli_connect("151.106.124.51","u188140722_instamojo","Admin@123","u188140722_instamojo");
 
-<?php
+
 $servername='151.106.124.51';
 $username="u188140722_instamojo";
 $password="Admin@123";
@@ -15,5 +15,4 @@ catch(PDOException $e)
 {
     echo '<br>'.$e->getMessage();
 }
-?>
 ?>
