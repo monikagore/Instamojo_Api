06084a32008318dfca40a9fed3ea58719d92fd89
monikagore <monikagore6@gmail.com>
2022-09-01 17:19:13 +0530

    m

--- config.php
+++ config.php
@@ -1,4 +1,19 @@
 <?php
 //$conn=mysqli_connect("151.106.124.51","u188140722_instamojo","Admin@123","u188140722_instamojo");
-$con=new PDO("mysql:host=$151.106.124.51;dbname=u188140722_instamojo","u188140722_instamojo","Admin@123");
+
+<?php
+$servername='151.106.124.51';
+$username="u188140722_instamojo";
+$password="Admin@123";
+try
+{
+    $con=new PDO("mysql:host=$servername;dbname=u188140722_instamojo",$username,$password);
+    $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //echo 'connected';
+}
+catch(PDOException $e)
+{
+    echo '<br>'.$e->getMessage();
+}
+?>
 ?>
