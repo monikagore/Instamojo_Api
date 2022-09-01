da1732f32df4b9bddb203acee4b7f1d02a8728d6
monikagore <monikagore6@gmail.com>
2022-09-01 16:40:06 +0530

    status

--- pay.php
+++ pay.php
@@ -7,11 +7,13 @@ const AUTH_TOKEN = "test_2fc07cf9f5dc135531d31997f39";
 $name=$_POST['name'];
 $purpose=$_POST['purpose'];
 $email=$_POST['email'];
-$amt=$_POST['amount'];
+$phone_number=$_POST['number'];
+$amount=$_POST['amount'];
+$status=$_POST['status'];
 if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
 {
     $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
-    $sql=mysqli_query($conn,"INSERT INTO `payment_details`(`name`, `purpose`, `email`, `phone_number`, `amount`) VALUES ('$name','$purpose','$email','554','$amt')");
+    $sql=mysqli_query($conn,"INSERT INTO `payment_details`(`name`, `purpose`, `email`, `phone_number`, `amount`) VALUES ('$name','$purpose','$email','$phone_number','$amount')");
     try {
         $response = $api->paymentRequestCreate(array(
             "purpose" => $_POST['purpose'],
@@ -21,6 +23,7 @@ if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) &
             "email" => $_POST['email'],
             "redirect_url" => "http://monika.tectignis.in/success.php"
             ));
+            echo
         header('Location:'. $response['longurl']);
          
     }
