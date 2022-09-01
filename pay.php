aab4266e28381880030b5b44421b342be5e732f4
monikagore <monikagore6@gmail.com>
2022-09-01 16:31:13 +0530

    pay page

--- pay.php
+++ pay.php
@@ -1,13 +1,17 @@
 <?php
 require('./instamojo.php');
+include('config.php');
 const API_KEY ="test_e3574b79601e4738061919e742c";
 const AUTH_TOKEN = "test_2fc07cf9f5dc135531d31997f39";
 
-
+$name=$_POST['name'];
+$purpose=$_POST['purpose'];
+$email=$_POST['email'];
+$amt=$_POST['amount'];
 if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
 {
     $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
-    
+    $sql=mysqli_query($conn,"INSERT INTO `payment_details`(`name`, `purpose`, `email`, `phone_number`, `amount`) VALUES ('$name','$purpose','$email','554','$amt')");
     try {
         $response = $api->paymentRequestCreate(array(
             "purpose" => $_POST['purpose'],
@@ -15,10 +19,10 @@ if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) &
             "amount" => $_POST['amount'],
             "send_email" => true,
             "email" => $_POST['email'],
-            "redirect_url" => "http://monika.tectignis.in/success.php",
-            "webhook" => "http://monika.tectignis.in/webhook.php"
+            "redirect_url" => "http://monika.tectignis.in/success.php"
             ));
         header('Location:'. $response['longurl']);
+         
     }
     catch (Exception $e) {
         print('Error: ' . $e->getMessage());
